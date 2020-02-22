fc69280c28cc308528645fbcb24233f99f85f1a0
alvinkim01 <55009265+alvinkim01@users.noreply.github.com>
2020-02-22 21:49:39 +0900

    Initiallize react project

---
+++ src/components/Texts.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+class Texts extends React.Component{
+    render(){
+        return(
+            <Card>
+                <CardContent>
+                    Text 에 대한 설명
+                </CardContent>
+            </Card>
+
+        );
+    }
+}
+
+export default Texts;
---
+++ src/components/Words.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+class Words extends React.Component{
+    render(){
+        return(
+            <Card>
+                <CardContent>
+                    Words 에 대한 설명
+                </CardContent>
+            </Card>
+
+        );
+    }
+}
+
+export default Words;
